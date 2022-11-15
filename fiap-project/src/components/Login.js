4613ff3b98e4c09de7e1a577a797503d3bc35dc7
Gustavo <gusttavo.developer@gmail.com>
2022-11-15 10:37:54 -0300

    [FIX] adjust mobile

--- fiap-project/src/components/Login.js
+++ fiap-project/src/components/Login.js
@@ -31,6 +31,7 @@ function Login() {
                 <img
                     src={banner}
                     alt="Logo"
+                    className='banner'
                 />
             </div>
 
